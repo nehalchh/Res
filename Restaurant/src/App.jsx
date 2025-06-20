02a3a99da51fe6b47273f2711a76c64f468714c6
Nehalchh <nehalchhalotre27@gmail.com>
2025-06-21 01:58:00 +0530

    con

--- Restaurant/src/App.jsx
+++ Restaurant/src/App.jsx
@@ -1,4 +1,4 @@
-import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
 
--- Restaurant/src/container/Gallery/Gallery.jsx
+++ Restaurant/src/container/Gallery/Gallery.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowrightShort} from "react-icons/bs";
+import {SubHeading} from "../../components";
+import {images} from "../../constants";
 
 import './Gallery.css';
 
